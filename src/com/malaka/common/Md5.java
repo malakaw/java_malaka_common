f2cf05803d697b67325289718bbb89d7d968af9c
malaka <malaka@10.101.3.24>
2014-06-04 10:01:29 +0800

    add reactor nio thread

--- src/com/malaka/common/Md5.java
+++ src/com/malaka/common/Md5.java
@@ -1,12 +1,27 @@
 package com.malaka.common;
+
+import lombok.Data;
+import lombok.extern.log4j.Log4j;
+
 /**
  *项目名：Malaka_java_common
  *包名：com.malaka.common
  *创建日期：2013-12-31
  *作者：malaka.weng@vipshop.com
  */
-public class Md5 {
 
+@Data
+public class Md5 {
 	
+	private String id;
+	private String name;
+	private String identity;
+	
+	
+	public static void main(String[] args) {
+		Md5 mm = new Md5();
+		mm.setId("22");
+		System.out.println(mm.getId());
+	}
 		
 }
